c959e0bb06b4be69b57a67f678f44ddd77bac8a8
arcy <edwardguevarra2003@gmail.com>
2017-08-22 11:51:34 +0800

    Arc

---
+++ platforms/browser/www/public/assets/member/js/receive_payment.js
@@ -0,0 +1,398 @@
+var receive_payment = new receive_payment();
+var maximum_payment = 0;
+var is_amount_receive_modified = false;
+
+function receive_payment()
+{
+	init();
+
+	function init()
+	{
+		document_ready();
+	}
+
+	function document_ready()
+	{
+		event_line_check_change();
+		event_payment_amount_change();
+		event_received_amount_change();
+		event_button_action_click();
+		action_initialize_load();
+		action_put_customers();
+		action_put_accounting();
+		action_put_payment();
+		test_function();
+	}
+	function test_function()
+	{
+	}
+	function action_put_payment()
+	{
+		get_payment_method(function(_payment_method)
+		{
+			$('.drop-down-payment').html('');
+			var ctr = 0;
+			$.each(_payment_method, function(key, value)
+			{
+				ctr++;
+				$('.drop-down-payment').globalDropList("reload");
+				var append = '<option value="'+value['payment_method_id']+'">'+value['payment_name']+'</option>';
+				$('.drop-down-payment').append(append);
+				if(_payment_method.length == ctr)
+				{
+					$('.drop-down-payment').globalDropList("reload");
+				}
+			});
+		});
+	}
+
+	function action_put_accounting()
+	{
+		get_all_coa(function(coa)
+		{
+			$('.drop-down-coa').html('');
+			var ctr = 0;
+			$.each(coa, function(index, val) 
+			{
+				ctr++;
+				var append = '<option value="'+val.account_id+'" indent="'+val.account_sublevel+'" add-search="" reference="">'+
+							 val.account_number+' • '+val.account_name+
+							 '</option>';
+				$('.drop-down-coa').append(append);
+				// console.log(append);
+				if (coa.length == ctr) 
+				{
+					initialize_select_plugin();
+				}
+			});
+		});
+	}
+
+	function action_put_customers()
+	{
+		get_all_customers(function(customer)
+		{
+			$.each(customer, function(index, val) 
+			{
+				var append = '<option value="'+val.customer_id+'" email="'+val.email+'">'+
+							 val.first_name+' '+val.middle_name+' '+val.last_name+
+							 '</option>';
+
+				$('.drop-down-customer').append(append);
+
+				if (customer.length == (index + 1)) 
+				{
+					initialize_dropdown_customer();
+				}
+			});
+		});
+	}
+
+	function initialize_dropdown_customer()
+	{
+		$(".drop-down-customer").globalDropList(
+		{
+		    hasPopup    : "false",
+		    width 		: "100%",
+		    placeholder : 'Customer',
+		    onChangeValue: function()
+		    {
+		    	var customer_id = $(this).val();
+		    	var check = $(".for-tablet-only").html();
+		    	// if(check == null || check == "")
+		    	// {
+			    // 	$(".tbody-item").load("/member/customer/load_rp/"+ (customer_id != '' ? customer_id : 0), function()
+			    // 	{
+			    // 		action_compute_maximum_amount();
+			    // 	})		    		
+		    	// }
+		    	// else
+	    		// {
+			    // 	$(".tbody-item").load("/tablet/customer/load_rp/"+ (customer_id != '' ? customer_id : 0), function()
+			    // 	{
+			    // 		action_compute_maximum_amount();
+			    // 	})		    		
+	    		// }
+	    		get_session('rp_id', function(rp_id)
+	    		{
+	    			if(!rp_id)
+	    			{
+			    		get_shop_id(function(shop_id)
+			    		{
+			    			db.transaction(function (tx)
+					        {
+					        	// $query->leftJoin(DB::raw("(select sum(rpline_amount) as amount_applied, rpline_reference_id from tbl_receive_payment_line as rpline inner join tbl_receive_payment rp on rp_id = rpline_rp_id where rp_shop_id = ".$shop_id." and rpline_reference_name = 'invoice' group by concat(rpline_reference_name,'-',rpline_reference_id)) pymnt"), "pymnt.rpline_reference_id", "=", "inv_id");
+					        	// $query->where("inv_shop_id", $shop_id)->where("inv_customer_id", $customer_id);
+					        	// ->where("inv_is_paid", 0)->where("is_sales_receipt",0)
+					            var query_check = 'SELECT * FROM tbl_customer_invoice '+
+					            				  'LEFT JOIN (SELECT sum(rpline_amount) as amount_applied, rpline_reference_id from tbl_receive_payment_line as rpline inner join tbl_receive_payment rp on rp_id = rpline_rp_id where rp_shop_id = '+shop_id+' and rpline_reference_name = "invoice" GROUP BY rpline_reference_id) ON rpline_reference_id = inv_id '+
+					            				  // 'LEFT JOIN "pymnt.rpline_reference_id", "=", "inv_id" '+
+					                              'WHERE inv_shop_id = '+shop_id+' AND inv_customer_id = '+customer_id+' AND inv_is_paid = 0 AND is_sales_receipt = 0';
+
+					            // var check    = 'SELECT * FROM tbl_customer_invoice LEFT JOIN';
+					            tx.executeSql(query_check, [], function(tx, results)
+					            {
+					                var append_default = '<tr class="inv-rp-id">'+
+				                                             '<input type="hidden" value="invoice" name="rpline_txn_type[]">'+
+				                                             '<input type="hidden" value="" name="rpline_txn_id[]" class="inv-rp-id">'+
+				                                              '<td class="text-center">'+
+				                                                '<input type="hidden" class="line-is-checked" name="line_is_checked[]" value="" >'+
+				                                                '<input type="checkbox" class="line-checked">'+
+				                                              '</td>'+
+				                                              '<td></td>'+
+				                                              '<td class="text-right"></td>'+
+				                                              '<td><input type="text" class="text-right original-amount" value="" disabled /></td>'+
+				                                              '<td><input type="text" class="text-right balance-due" value="" disabled /></td>'+
+				                                              '<td><input type="text" class="text-right amount-payment" name="rpline_amount[]" value=""/></td>'+
+				                                          '</tr>';
+					                if (results.rows.length <= 0) 
+					                {
+					                	$('.tbody-item').html(append_default);
+					                }
+					                else
+					                {
+					                	$('.tbody-item').html('');
+
+					                	$.each(results.rows, function(index, val) 
+						                {
+						                	get_cm_amount(val.credit_memo_id, function(cm_amount)
+					                		{
+					                			// console.log(cm_amount);
+					                			var append = '<tr class="inv-rp-id" inv_rp_id="'+val.inv_id+'">'+
+					                                             '<input type="hidden" value="invoice" class="rp-type" name="rpline_txn_type[]">'+
+					                                             '<input type="hidden" value="'+val.inv_id+'" class="rp-inv-id" name="rpline_txn_id[]">'+
+					                                              '<td class="text-center">'+
+					                                                '<input type="hidden" class="line-is-checked inv-is-paid" name="line_is_checked[]" value="" >'+
+					                                                '<input type="checkbox" class="rp line-checked">'+
+					                                              '</td>'+
+					                                              '<td>Invoice #'+val.new_inv_id+' ( '+val.inv_date+' )</td>'+
+					                                              '<td class="text-right">'+val.inv_date+'</td>'+
+					                                              '<td><input type="text" class="text-right original-amount" value="'+(val.inv_overall_price).toFixed(2)+'" disabled /></td>'+
+					                                              '<td><input type="text" class="text-right balance-due" value="'+(((val.inv_overall_price) - val.amount_applied) + ((val.rpline_amount ? val.rpline_amount : 0 ) - cm_amount)).toFixed(2)+'" disabled /></td>'+
+					                                              '<td><input type="text" class="text-right amount-payment" name="rpline_amount[]" value=""/></td>'+
+					                                          '</tr>';
+
+							                	$('.tbody-item').append(append);
+					                		});
+						                	
+						                });
+					                }			                
+					            },
+					            onError);
+					        });
+			    		});
+	    			}
+	    		});
+		    }
+		});
+	}
+
+	this.action_initialize_load = function()
+	{
+		action_initialize_load();
+	}
+
+	function action_initialize_load()
+	{
+		// initialize_select_plugin();
+		$(".datepicker").datepicker();
+		$(".amount-payment").change();
+	}
+
+	function initialize_select_plugin()
+	{
+		$(".drop-down-payment").globalDropList(
+		{
+		    hasPopup    : "false",
+		    width 		: "100%",
+		    placeholder : 'Payment Method'
+		});
+
+		$(".drop-down-coa").globalDropList(
+		{
+		    hasPopup    : "false",
+		    width 		: "100%",
+		    placeholder : 'Account'
+		});
+	}
+
+	/* CHECK BOX FOR LINE ITEM */
+	function event_line_check_change()
+	{
+		$(document).on("change", ".line-checked", function()
+		{
+			action_change_input_value($(this));
+		});
+	}
+	function action_change_input_value($this)
+	{
+		if($this.is(":checked"))
+		{
+			$this.prev().val(1);
+			var balance = $this.parents("tr").find(".balance-due").val();
+			if(!formatFloat($this.parents("tr").find(".amount-payment").val()) > 0)
+			{
+				$this.parents("tr").find(".amount-payment").val(balance).change();
+			}
+		}
+		else
+		{
+			$this.prev().val(0);
+			if(formatFloat($this.parents("tr").find(".amount-payment").val()) > 0)
+			$this.parents("tr").find(".amount-payment").val('').change();
+		}
+	}
+
+	function action_compute_maximum_amount()
+	{
+		$(".balance-due").each(function()
+		{
+			maximum_payment += formatFloat($(this).val());
+		})
+	}
+
+	function event_received_amount_change()
+	{
+		$(document).on("change", ".amount-received", function()
+		{
+			$(this).val(formatMoney($(this).val()));
+
+			var amount_receive = formatFloat($(this).val());
+			var amount_applied = formatFloat(action_total_amount_apply());
+
+			if( amount_receive > amount_applied)
+			{
+				console.log("true");
+				action_update_credit_amount(amount_receive - amount_applied);
+			}
+			else
+			{
+				action_update_credit_amount(0)
+			}
+		})
+
+		$(document).on("keydown", ".amount-received", function()
+		{
+			is_amount_receive_modified = true;
+		})
+	}
+
+	function action_update_apply_amount($amount)
+	{
+		$(".amount-to-apply").val($amount);
+		$(".amount-apply").html("PHP "+formatMoney($amount))
+	}
+
+	function action_update_credit_amount($amount)
+	{
+		$(".amount-to-credit").val($amount);
+		$(".amount-credit").html("PHP "+formatMoney($amount))
+	}
+
+	function event_payment_amount_change()
+	{
+		$(document).on("change",".amount-payment", function(e)
+		{
+			$(this).val(formatFloat($(this).val()) == 0 ? '' : formatMoney($(this).val()));
+
+			!is_amount_receive_modified ? $(".amount-received").val(action_total_amount_apply()).change() : $(".amount-received").change();
+			action_update_apply_amount(action_total_amount_apply());
+
+			// check - uncheck checkbox
+			if(formatFloat($(this).val()) > 0)
+			{
+				$(this).parents("tr").find(".line-checked").prop("checked", true).change();
+			}
+			else
+			{
+				$(this).parents("tr").find(".line-checked").prop("checked", false).change();
+			}
+
+			// validation for exceeding balace
+			if(formatFloat($(this).val()) > formatFloat($(this).parents("tr").find(".balance-due").val()) )
+			{
+				this.setCustomValidity("You may not pay more than the balance due");
+    			return false;
+			}
+			else
+			{
+				this.setCustomValidity("");
+    			return true;
+			}
+		})
+	}
+
+	function action_total_amount_apply()
+	{
+		var line_payment_amount = 0;
+		$(".amount-payment").each(function()
+		{
+			line_payment_amount += formatFloat($(this).val());
+		})
+		return formatMoney(line_payment_amount);
+	}
+
+	function formatFloat($this)
+	{
+		return Number($this.toString().replace(/[^0-9\.]+/g,""));
+	}
+
+	function formatMoney($this)
+	{
+		var n = formatFloat($this), 
+	    c = isNaN(c = Math.abs(c)) ? 2 : c, 
+	    d = d == undefined ? "." : d, 
+	    t = t == undefined ? "," : t, 
+	    s = n < 0 ? "-" : "", 
+	    i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))), 
+	    j = (j = i.length) > 3 ? j % 3 : 0;
+	   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+	}
+
+	function event_button_action_click()
+	{
+		$(document).on("click","button[type='submit']", function()
+		{
+			$(".button-action").val($(this).attr("data-action"));
+		})
+	}
+
+}
+
+function submit_done(data)
+{
+	if(data.status == "success" || data.response_status == "success")
+	{
+		if(data.type == "payment_method")
+		{
+			$(".drop-down-payment").load("/member/maintenance/load_payment_method", function()
+			{
+				$(this).globalDropList("reload");
+				$(this).val(data.payment_method_id).change();
+			});
+			data.element.modal("toggle");
+		}
+		else if(data.type == "account")
+		{
+			$(".drop-down-coa").load("/member/accounting/load_coa?filter[]=Bank", function()
+			{
+				$(this).globalDropList("reload");
+				$(this).val(data.id).change();
+			});
+			data.element.modal("toggle");
+		}
+		else if(data.redirect)
+        {
+        	toastr.success(data.message);
+        	location.href = data.redirect;
+    	}
+    	else
+    	{
+    		$(".rcvpymnt-container").load(data.url+" .rcvpymnt-container .rcvpymnt-load-data", function()
+			{
+				receive_payment.action_initialize_load();
+				toastr.success(data.message);
+			});
+    	}
+	}
+}
---
+++ platforms/browser/www/public/assets/tablet/sales_receipt.js
@@ -0,0 +1,123 @@
+var sales_receipt = new sales_receipt();
+var db = openDatabase("my168shop", "1.0", "Address Book", 200000); 
+var query = "";
+var dataset_from_browser = null; 
+function sales_receipt()
+{
+	init();
+
+	function init()
+	{
+		$(document).ready(function()
+		{
+			document_ready();
+		});
+	}
+	function document_ready()
+	{
+		check_if_have_login();
+	}
+	function check_if_have_login()
+	{
+        db.transaction(function (tx)
+        {
+            var query_check = 'SELECT * from tbl_agent_logon LIMIT 1';            
+            tx.executeSql(query_check, [], function(tx, results)
+            {
+                if(results.rows.length <= 0)
+                {
+                    location.href = "login.html";
+                }
+                else
+                {
+                	action_get_agent(results.rows);
+                }
+            });
+        });
+	}
+	function action_get_agent(data)
+	{
+		db.transaction(function (tx)
+        {
+        	var query_check = 'SELECT * from tbl_employee LEFT JOIN tbl_position ON tbl_employee.position_id = tbl_position.position_id where employee_id = "'+data[0]['agent_id']+'" ';            
+            tx.executeSql(query_check, [], function(tx, results)
+            {
+            	data_result = results.rows;
+				$(".employee-name").html(data_result[0]['first_name']+" "+data_result[0]['middle_name']+" "+data_result[0]['last_name']);
+				$(".employee-position").html(data_result[0]['position_name']);
+                action_get_sir(data[0]['agent_id']);
+            });
+        });
+	}
+	function action_get_sir(agent_id)
+	{
+		db.transaction(function (tx)
+        {
+        	var query_check = 'SELECT * from tbl_sir where sales_agent_id = "'+agent_id+'" AND lof_status IN ("1","2") AND sir_status IN ("0","1")';
+        	var sir_id = "";       
+            tx.executeSql(query_check, [], function(txs, results)
+            {
+            	data_result = results.rows;
+        		$(data_result).each(function(key, datarow)
+				{	
+					$(".select-tag-sir").append("<option value='"+datarow['sir_id']+"'>SIR#"+datarow['sir_id']+"</option>");
+				});
+
+				$(".sir-no").html(data_result[0]['sir_id']);
+				sir_id = data_result[0]['sir_id'];
+
+				var query_update = 'UPDATE tbl_agent_logon SET selected_sir = "'+sir_id+'" where agent_id = "'+agent_id+'"';            
+	            tx.executeSql(query_update, [], function(tx, results_update)
+	            {
+	            	console.log("update success");
+	            });
+
+                get_invoice_list(sir_id);           
+            });        	
+        });
+	}
+    function get_invoice_list(sir_id)
+    {
+        db.transaction(function (tx)
+        {
+            var query_invoice_list = 'SELECT * FROM tbl_manual_invoice LEFT JOIN tbl_sir ON tbl_sir.sir_id = tbl_manual_invoice.sir_id LEFT JOIN tbl_customer_invoice ON tbl_customer_invoice.inv_id = tbl_manual_invoice.inv_id LEFT JOIN tbl_credit_memo ON tbl_credit_memo.cm_id = tbl_customer_invoice.credit_memo_id LEFT JOIN tbl_customer ON tbl_customer.customer_id = tbl_customer_invoice.inv_customer_id WHERE tbl_customer_invoice.is_sales_receipt = "1" and tbl_manual_invoice.sir_id = "'+sir_id+'" GROUP BY tbl_customer_invoice.inv_id';
+            tx.executeSql(query_invoice_list, [], function(txst, results)
+            {
+                data_result = results.rows;
+                var tr = "";
+                $(data_result).each(function(key, datarow)
+                {   
+                    tr += '<tr><td>'+datarow['inv_id']+'</td>';
+                    checked = datarow['inv_is_paid'] == 1 ? 'checked' : '' ;
+                    company = datarow['company'] == "" ? datarow['first_name'] +" "+ datarow['last_name'] : "" ;
+                    tr += '<td class="text-center"><input type="checkbox" name="paid" disabled '+checked+'></td>';
+                    tr += '<td>'+company+'</td>';
+                    cm_amount = datarow['cm_amount'];
+                    inv_total_amount = datarow['inv_overall_price'];
+                    if(cm_amount)
+                    {
+                        inv_total_amount = datarow['inv_overall_price'] - cm_amount;
+                    }
+                    tr += '<td>'+ReplaceNumberWithCommas((inv_total_amount).toFixed(2))+'</td>';
+                    tr += '<td class="text-center"><div class="btn-group">'+
+                          '<button type="button" class="btn btn-sm btn-custom-white dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Action <span class="caret"></span></button>'+
+                          '<ul class="dropdown-menu dropdown-menu-custom">'+
+                          '<li><a onClick="edit_invoice('+datarow['inv_id']+')">Edit Invoice</a></li>'+
+                          ' <li><a onClick="view_invoice('+datarow['inv_id']+')">View Receipt</a></li></ul></div></td>';
+                    tr += '</tr>';
+                });
+                $(".tbody-invoice-list").append(tr);
+            });
+        });
+    }
+  
+}
+function ReplaceNumberWithCommas(yourNumber)
+{
+    //Seperates the components of the number
+    var n= yourNumber.toString().split(".");
+    //Comma-fies the first part
+    n[0] = n[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    //Combines the two sections
+    return n.join(".");
+}
